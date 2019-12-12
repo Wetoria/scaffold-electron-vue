c5121ac8135d713e89e3f19b79940930202f5692
Wetoria <vwetoria@gmail.com>
2019-12-12 14:04:18 +0800

    [IMP]修改目录结构 & alias

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
 module.exports = {
   publicPath: './',
   assetsDir: '.',
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('@components', resolve('src/common/components'))
+      .set('@pages', resolve('src/pages'))
+      .set('@common', resolve('src/common'))
+      .set('@assets', resolve('src/common/assets'));
+  },
 };
